240a6c455831d4ac94cb9d2cef71bc29e8f7df4e
S1ddhart4 <superflydropkick37@gmail.com>
2024-08-29 13:36:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1st programm
+print((9 ** 0.5) * 5)
+# 2nd programm
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd programm
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+# 4th programm
+print(type('123.456'))
+print(type(int(float('123.456'))))
+print(123.456 * 10)
