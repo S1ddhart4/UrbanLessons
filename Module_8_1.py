03018567e1f474ba0b684096b1b60e5284101d4f
John Constantine <superflydropkick37@gmail.com>
2024-12-16 18:14:43 +0300

    Update Module_8_1.py

--- Module_8_1.py
+++ Module_8_1.py
@@ -1,13 +1,11 @@
 def add_everything_up(a, b):
     try:
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            return a + b
-        elif isinstance(a, str) and isinstance(b, str):
-            return a + b
-        else:
-            raise TypeError
+        result = a + b
+        if isinstance(result, float):
+            return round(result, 3)
+        return result
     except TypeError:
-        return f"{a}{b}"
-print(add_everything_up(123.456, 'строка'))  # 123.456строка
-print(add_everything_up('яблоко', 4215))     # яблоко4215
-print(add_everything_up(123.456, 7))         # 130.456
+        return str(a) + str(b)
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
