76cbefc567f18ef795329358d9dc9ed5ceaee783
John Constantine <superflydropkick37@gmail.com>
2024-12-16 18:02:58 +0300

    Update Module_8_2.py

--- Module_8_2.py
+++ Module_8_2.py
@@ -3,18 +3,14 @@ def personal_sum(numbers):
     incorrect_data = 0
     for item in numbers:
         try:
-            if isinstance(item, (int, float)):
-                result += item
-            else:
-                raise TypeError
+            result += item
         except TypeError:
             print(f"Некорректный тип данных для подсчёта суммы - {item}")
             incorrect_data += 1
     return result, incorrect_data
 def calculate_average(numbers):
     try:
-        if not isinstance(numbers, (list, tuple, set)):
-            raise TypeError("Некорректный тип данных")
+        iter(numbers)
         total_sum, incorrect_data = personal_sum(numbers)
         count = len(numbers) - incorrect_data
         return total_sum / count if count > 0 else 0
@@ -23,7 +19,7 @@ def calculate_average(numbers):
     except TypeError:
         print("В numbers записан некорректный тип данных")
         return None
-print(f'Result 1: {calculate_average("1, 2, 3")}')
-print(f'Result 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
-print(f'Result 3: {calculate_average(567)}')
-print(f'Result 4: {calculate_average([42, 15, 36, 13])}')
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
